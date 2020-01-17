76193e818ce1d9793d98422bd3548c898ef5f0c7
senjansen <senjansen.sj@gmail.com>
2020-01-17 12:47:28 +0700

    awesome landing page done

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <h1>{{ message }}</h1>
+  <div>
+    <landingPage></landingPage>
+  </div>  
 </template>
 
 <script>
+import landingPage from './components/landingPage'
+import Vue from 'vue'
+
 export default {
+  components: {
+    landingPage
+  },
   data() {
     return {
-      message: 'Hello world',
+      message: 'Hello world'
     }
-  },
+  }
 }
 </script>
 
